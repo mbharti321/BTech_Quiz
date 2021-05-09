7ec395459118d115c7db2cc49ea80f52edfbea1b
mbharti321 <mbharti321@gmail.com>
2021-05-09 18:18:32 +0530

    add menu responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ var menuList = document.getElementById("menuList");
 menuList.style.maxHeight = "0px";
 function togglemenu(){
     if(menuList.style.maxHeight == "0px"){
-        menuList.style.maxHeight = "130px";
+        menuList.style.maxHeight = "400px";
     }
     else{
         menuList.style.maxHeight = "0px";
