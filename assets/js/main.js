2320b96e85736c94455099372bf8276252468761
mbharti321 <mbharti321@gmail.com>
2021-05-10 14:19:55 +0530

    add quiz taking whole process

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+//loading users json data
+//include the   'async':false   parameter or the object data won't get captured when loading
+var json = $.getJSON({ 'url': "users.json", 'async': false });
+//The next line of code will filter out all the unwanted data from the object.
+json = JSON.parse(json.responseText);
+var userData = json["users"];
+
+//loading questions data
+var json2 = $.getJSON({ 'url': "questions.json", 'async': false });
+//The next line of code will filter out all the unwanted data from the object.
+json2 = JSON.parse(json2.responseText);
+var questions = json2["questions"];
+
+
 //for menu responsiveness
 var menuList = document.getElementById("menuList");
 menuList.style.maxHeight = "0px";
@@ -17,12 +31,7 @@ $(document).ready(function () {
 });
 
 
-//loading users json data
-//include the   'async':false   parameter or the object data won't get captured when loading
-var json = $.getJSON({ 'url': "users.json", 'async': false });
-//The next line of code will filter out all the unwanted data from the object.
-json = JSON.parse(json.responseText);
-var userData = json["users"];
+
 
 
 
@@ -50,15 +59,188 @@ function validateUser() {
     return false;
 }
 
+//function to valited if user checked the "I agree checkbox"
 function validateCheck() {
     var rulesCheck = document.getElementById("rulesCheck");
     console.log(rulesCheck.checked);
     if (rulesCheck.checked) {
-        $("#content").load("quiz-questions.html");        
+        $("#content").load("quiz-questions.html");
     }
     else {
         alert("Please select \"I agree\"!!!");
     }
     return false;
 }
+
+
+
+
+var currQuestionNum = 0;
+var currAnswer = "";
+let userAnswers = [];
+var correctCount = 0;
+
+//function
+function loadFirstQuestion() {
+    var currQuestion = questions[0]["question"];
+    currAnswer = questions[0]["ans"];
+    var currOptions = questions[0]["options"];
+
+    document.getElementById("question").innerHTML = currQuestion;
+    document.getElementById("option1").value = currOptions[0];
+    document.getElementById("option2").value = currOptions[1];
+    document.getElementById("option3").value = currOptions[2];
+    document.getElementById("option4").value = currOptions[3];
+    document.getElementById("option1Lable").innerHTML = currOptions[0];
+    document.getElementById("option2Lable").innerHTML = currOptions[1];
+    document.getElementById("option3Lable").innerHTML = currOptions[2];
+    document.getElementById("option4Lable").innerHTML = currOptions[3];
+}
+
+
+//function
+function loadNextQuestion() {
+    if (!validateAnswer()) {
+        return false;
+    }
+    currQuestionNum += 1;
+
+    var submitBtn = document.getElementById("submitBtn");
+    var nextBtn = document.getElementById("nextBtn");
+
+    // if (currQuestionNum < questions.length - 1) {
+    //     currQuestionNum += 1;
+    // } 
+    if (currQuestionNum == questions.length - 1) {
+        submitBtn.style.display = "block";
+        submitBtn.style.background = "#3371e2";
+        submitBtn.style.color = "#FFF";
+        nextBtn.style.display = "none";
+        // alert("This is the last Question.!!");
+    }
+
+
+
+    var currQuestion = questions[currQuestionNum]["question"];
+    currAnswer = questions[currQuestionNum]["ans"];
+    var currOptions = questions[currQuestionNum]["options"];
+
+
+    document.getElementById("question").innerHTML = currQuestion;
+    document.getElementById("option1").value = currOptions[0];
+    document.getElementById("option2").value = currOptions[1];
+    document.getElementById("option3").value = currOptions[2];
+    document.getElementById("option4").value = currOptions[3];
+    document.getElementById("option1Lable").innerHTML = currOptions[0];
+    document.getElementById("option2Lable").innerHTML = currOptions[1];
+    document.getElementById("option3Lable").innerHTML = currOptions[2];
+    document.getElementById("option4Lable").innerHTML = currOptions[3];
+
+    
+    clearAllRadioBtn();
+
+}
+
+
+//function
+function loadPreviousQuestion() {
+    var submitBtn = document.getElementById("submitBtn");
+    var nextBtn = document.getElementById("nextBtn");
+
+    if (currQuestionNum > 0) {
+        currQuestionNum -= 1;
+        submitBtn.style.display = "none";
+        nextBtn.style.display = "block";
+    }
+    else {
+        alert("This is the first Question.!!");
+        return false;
+    }
+    var currQuestion = questions[currQuestionNum]["question"];
+    currAnswer = questions[currQuestionNum]["ans"];
+    var currOptions = questions[currQuestionNum]["options"];
+
+    document.getElementById("question").innerHTML = currQuestion;
+    document.getElementById("option1").value = currOptions[0];
+    document.getElementById("option2").value = currOptions[1];
+    document.getElementById("option3").value = currOptions[2];
+    document.getElementById("option4").value = currOptions[3];
+    document.getElementById("option1Lable").innerHTML = currOptions[0];
+    document.getElementById("option2Lable").innerHTML = currOptions[1];
+    document.getElementById("option3Lable").innerHTML = currOptions[2];
+    document.getElementById("option4Lable").innerHTML = currOptions[3];
+    // clearAllRadioBtn();
+}
+
+
+function validateAnswer() {
+    var selectedAnswer = document.querySelector('input[name="option"]:checked');
+
+    if (selectedAnswer != null) {
+        // document.getElementById("optionDetail").innerHTML = selectedAnswer.value + "  option is selected";
+        // alert(selectedAnswer.value + "  option is selected");
+        var isCorrect = false;
+        if (selectedAnswer.value == currAnswer) {
+            isCorrect = true;            
+            correctCount += 1;
+        }
+        // 
+        userAnswers.push([currQuestionNum + 1, isCorrect, selectedAnswer.value, currAnswer]);
+        console.log(userAnswers);
+        return true;
+    }
+    else {
+        alert("No option Selected!\nPlease look back to this question..");
+        // return false;
+        return true;
+    }
+
+}
+
+//to clear radio button selection
+function clearAllRadioBtn() {
+    try {
+        if(document.querySelector('input[name="option"]:checked').checked != false){
+            document.querySelector('input[name="option"]:checked').checked = false;
+        }
+      }
+      catch(err) {
+        console.log(err.message);
+      }
+    
+}
+
+
+function loadResultPage() {
+    if (!validateAnswer()) {
+        return false;
+    }
+    $("#content").load("quiz-result.html");
+}
+
+
+function showUserResult() {
+
+    document.getElementById("totalCount").innerHTML = questions.length;
+    document.getElementById("correctCount").innerHTML = correctCount;
+    document.getElementById("correctPercent").innerHTML = correctCount / questions.length * 100 + " %";
+  
+
+    $(document).ready(function () {
+        for (var i in userAnswers) {
+            var element = document.createElement("p");
+            element.innerHTML = userAnswers[i];
+            $("#resultDiv").append(element);
+        }
+    });
+}
+
+
+function showAnswers() {
+
+}
+function initializeUserAnswerMap() {
+
+}
+
 
